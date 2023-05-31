addca7ee9d8ba7993adc9442c34358aeffeaa37b
Magnus <magnus.sagerup@gmail.com>
2023-05-31 21:44:26 +0200

    added library, continue tomorrow...

--- FoundMe.sol
+++ FoundMe.sol
@@ -1,41 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./PriceConverter.sol";
 
 contract FoundMe {
+    using PriceConverter for uint256;
 
     uint256 public minUsd = 50 *1e18; // 1*10  ** 18 ;
     address[] public founders;
     mapping(address => uint256) public addressToAmountFounded;
 
     function found() public payable  {
-        // I want to be able to set a minimum found amount in USD.
-        // 1. How do I send ETH to this contract?
-
-        // This revers if condition is not meet.
-        require(getConvertionRate(msg.value) >= minUsd, "You dind't send enough, brokie.."); // 1e18 = 1 * 10 ** 18 = 1000000000000000000 = 1ETH
+        // When using a library, the first values before the method is the first param. so here, msg.value is the param for getConvertionRate function.
+        require(msg.value.getConvertionRate() >= minUsd, "You dind't send enough, brokie.."); // 1e18 = 1 * 10 ** 18 = 1000000000000000000 = 1ETH
         founders.push(msg.sender);
         addressToAmountFounded[msg.sender] = msg.value;
     }
 
-    function getPrice() public view returns(uint256) {
-        // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306 Sepolia testnet
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-         (,int answer,,,) = priceFeed.latestRoundData();
-        
-        return uint256(answer * 1e10);
-    }
-
-    function getConvertionRate(uint256 ethAmount) public view returns(uint256) {
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
-
-        return ethAmountInUsd;
-    }
-
     function withdraw() public {
-
+        // For loop
+        for(uint256 founderIndex = 0; founderIndex < founders.length; founderIndex++){
+            address founder = founders[founderIndex];
+            addressToAmountFounded[founder] = 0;
+        }
     }
 
 }
+
+https://youtu.be/gyMwXuJrbJQ?t=16829
